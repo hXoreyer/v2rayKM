6f9340582be2a7ad9fb1c7ac4c05160a3a17822f
arthur <arthur.he@anketitech>
2024-06-04 14:56:40 +0800

    添加自定标题栏

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,55 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import "./App.css"
+import iconSVG from "./assets/images/icon.svg"
+import minSVG from "./assets/images/min.svg"
+import maxSVG from "./assets/images/max.svg"
+import closeSVG from "./assets/images/close.svg"
+import Index from "."
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+  const handleMinimize = () => {
+    window.runtime.WindowMinimise()
+  }
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  const handleMaximize = () => {
+    window.runtime.WindowToggleMaximise()
+  }
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+  const handleClose = () => {
+    window.runtime.Quit()
+  }
+  return (
+    <div id="App">
+      <div
+        id="title"
+        className="bg-white text-black p-2 flex justify-between shadow-md"
+        style={{ "--wails-draggable": "drag", userSelect: "none" }}
+      >
+        <div className="ml-1 flex items-center gap-2">
+          <img src={iconSVG} className="w-6 h-6" alt="logo"></img>
+          <div className="text-sm font-bold">v2rayKM</div>
         </div>
-    )
+        <div
+          className="flex gap-2 mr-1 items-center"
+          style={{ "--wails-draggable": "no-drag", userSelect: "none" }}
+        >
+          <img
+            src={minSVG}
+            onClick={handleMinimize}
+            className="title_img"
+          ></img>
+          <img
+            src={maxSVG}
+            onClick={handleMaximize}
+            className="title_img"
+          ></img>
+          <img src={closeSVG} onClick={handleClose} className="title_img"></img>
+        </div>
+      </div>
+      <div>
+        <Index></Index>
+      </div>
+    </div>
+  )
 }
 
 export default App
